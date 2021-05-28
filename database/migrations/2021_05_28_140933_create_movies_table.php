3efda2318d9ef581a9bfcc84c7d4f88d877191c7
kariukirobert <44176348+kariukirobert@users.noreply.github.com>
2021-05-28 17:22:40 +0300

    Create 2021_05_28_140933_create_movies_table.php

---
+++ database/migrations/2021_05_28_140933_create_movies_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->integer('tmdb_id');
+            $table->string('title');
+            $table->double('vote_average');
+            $table->integer('vote_count');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+}
